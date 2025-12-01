a3563b76edfa362e874d7997fb578228cfc4dd8f
Nameless-Dev0 <youssef.samir.mls@gmail.com>
2025-12-01 23:29:32 +0200

    fix: free client structs after handling to prevent memory leaks

--- src/server.c
+++ src/server.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include "server_util.h"
+#include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include "server_util.h"
 
 int main() {
     int server_socket_fd = setup_server("127.0.0.1", 1500);
     while(true){
-        struct accepted_socket* client_socket = accept_incoming_connection(server_socket_fd);
-        receive_and_print(client_socket);
+        client_t* incoming_client = accept_incoming_connection(server_socket_fd);
+        receive_and_print(incoming_client);
+        free(incoming_client);
     }
     
     shutdown(server_socket_fd, SHUT_RDWR);
