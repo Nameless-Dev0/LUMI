1c04853d32f8f5f64293b1c635c6a3dc126be32a
Nameless-Dev0 <youssef.samir.mls@gmail.com>
2025-12-01 23:05:30 +0200

    fix: use stack-alloc sockaddr_in & deep-copy client address

--- src/server_util.c
+++ src/server_util.c
@@ -9,54 +9,62 @@
 #include "server_util.h"
 
 int setup_server(char *ip, int port){
-    //Socket Creation
+
     int server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    //Address Creation
-    struct sockaddr_in *socket_address = malloc(sizeof(struct sockaddr_in));
-    socket_address->sin_port = htons(port);
-    socket_address->sin_family = AF_INET;
+    struct sockaddr_in *server_address = malloc(sizeof(struct sockaddr_in));
+    server_address->sin_port = htons(port);
+    server_address->sin_family = AF_INET;
     
     if(strlen(ip) == 0)
-        socket_address->sin_addr.s_addr = INADDR_ANY;
+        server_address->sin_addr.s_addr = INADDR_ANY;
     else
-        inet_pton(AF_INET, ip, (struct sockaddr*)&socket_address->sin_addr.s_addr);
-    struct sockaddr_in *server_address = socket_address;
-    //Binding and Validation
+        inet_pton(AF_INET, ip, &server_address->sin_addr.s_addr);
+
     if(bind(server_socket_fd, (struct sockaddr*)server_address, sizeof(*server_address)) == 0)
-        printf("Socket bond to server successfully!\n");
-    //Listening and Validation
+        printf("Socket bound to server successfully!\n");
+
     if(listen(server_socket_fd, 10) == 0)
         printf("Server is Listening!\n");
+
     return server_socket_fd;
 }
 
-struct accepted_socket* accept_incoming_connection(int server_socket_fd){
-    struct sockaddr_in *client_address = malloc(sizeof(struct sockaddr_in)); 
+client_t* accept_incoming_connection(int server_socket_fd){
+    struct sockaddr_in client_address;
     socklen_t address_size = sizeof(struct sockaddr_in);
-    int client_socket_fd = accept(server_socket_fd, (struct sockaddr*)client_address, &address_size);
-    struct accepted_socket* socket_accepted = malloc(sizeof(struct accepted_socket));
-    socket_accepted->accepted_socket_fd = client_socket_fd;
-    socket_accepted->address = *client_address;
-    if(client_socket_fd < 0){
-        socket_accepted->error = client_socket_fd;
+    int accepted_client_fd = accept(server_socket_fd, (struct sockaddr*)&client_address, &address_size);
+    client_t* accepted_client = malloc(sizeof(client_t));
+
+    accepted_client->sock_fd = accepted_client_fd;
+    accepted_client->address = client_address;
+
+    if(accepted_client_fd < 0){
+        accepted_client->error = accepted_client_fd;
         printf("Socket Acceptance Falied!\n");
     }
     else
         printf("Client Connected:\n");
-    return socket_accepted;
+
+    return accepted_client;
 }
 
-void receive_and_print(struct accepted_socket* client_socket){
-    //Receiving and Printing
+void receive_and_print(client_t* client){
     char buffer[1024];
-    while(true)
-    {
-            size_t amount_received = recv(client_socket->accepted_socket_fd, buffer, 1024, 0);
-            if(amount_received > 0){
-                buffer[amount_received] = 0;
-                printf("Response was %s\n ", buffer);
-            }
-            if(amount_received == 0)
-                break;
-    }   
+    ssize_t amount_received;
+
+    while(1){
+        amount_received = recv(client->sock_fd, buffer, sizeof(buffer) - 1, 0);
+        if(amount_received == -1){
+            perror("receive failed!");
+            break;
+        }
+        if(amount_received == 0){
+            printf("client disconnected!\n");
+            break;
+        }
+
+        buffer[amount_received] = '\0';
+        printf("Response: %s\n", buffer);
+
+    }
 }
