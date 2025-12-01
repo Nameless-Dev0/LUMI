6a34fe831fd44bc129decb1fcdbcff7c20f3795b
Abdelrahman Gabriel <abdelrahman.gabriel10@gmail.com>
2025-12-01 17:17:52 +0200

    added multiple client sessions on the same server

--- server.c
+++ server.c
@@ -2,17 +2,19 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include "ServerUtils.h"
+#include <stdbool.h>
 
 
 int main() {
 
     int ServerSocketFD = SetupServer("127.0.0.1", 1500);
 
+    while(true){
     struct AcceptedSocket* ClientSocket = AcceptIncomingConnection(ServerSocketFD);
 
     ReceiveAndPrint(ClientSocket);
-
-    close(ClientSocket->AcceptedSocketFD);
+    }
+    
     shutdown(ServerSocketFD, SHUT_RDWR);
 
 
